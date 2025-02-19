c6ecfa57dbbaab8ef92e4f2e986e8b0610a91ead
Marc Cortés <marc.cortes@enti.cat>
2025-02-19 11:20:24 +0100

    Juego de dados

---
+++ rollmania/src/Die.js
@@ -0,0 +1,21 @@
+import './Die.js';
+
+import { useState } from 'react';
+
+function Die() {
+
+	let [number, setNumber] = useState(1);
+
+	function roll () {
+	let r = Math.floor(Math.random() * 6) + 1
+	setNumber(r);
+	}
+
+ 	return (
+ 		<button className="Die" onClick={roll}>
+ 		<p className="DieNumber">{number}</p>
+		</button>
+ );
+}
+
+export default Die;
---
+++ rollmania/src/Title.js
@@ -0,0 +1,12 @@
+import './Title.css';
+import Die from './Die.js';
+
+function Title() {
+ 	return (
+ 		<main className="Title">
+		<h1>Rollmania!</h1>
+ 		</main>
+ );
+}
+
+export default Title;
